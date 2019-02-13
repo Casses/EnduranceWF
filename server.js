e88a587928b734c61b400a4b35238fa44c9a93e4
Casses <al.bechard@gmail.com>
2019-02-13 00:50:55 -0500

    caps!

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 console.log(__dirname);
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/endurancewf'));
+app.use(express.static(__dirname + '/dist/EnduranceWF'));
 
 app.get('/*', function(req,res) {
     
-    res.sendFile(path.join(__dirname+'/dist/endurancewf/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/EnduranceWF/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
